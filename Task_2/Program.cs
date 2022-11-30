17bcccdd3bf41f098aa1da56a32c7327a35be688
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-11-30 12:16:45 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b>max)max=b;
+if (c>max)max=c;
+
+Console.WriteLine("Наибольшее число: {0}", max);
