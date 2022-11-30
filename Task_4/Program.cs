8132c4c1c69aa11626726fea16935d042c06f4fe
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-12-01 00:29:32 +0300

    Task_4.1

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -3,5 +3,7 @@ int N = int.Parse(Console.ReadLine());
     
 for (int i = 1; i <= N; i++)
     if (i % 2 == 0)
-        Console.Write ("{0}, ", i);
-        Console.Write ("чётные числа");
+    {
+                Console.Write ("{0}, ", i);
+    }
+Console.Write ("чётные числа");
