5423d5bbdf9c0a6c072977b556cb02a1284c527b
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-11-30 12:29:02 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+
+int numbers = int.Parse(Console.ReadLine());
+
+if ((numbers % 2) == 0)
+{
+    Console.WriteLine("Число {0} является четным ",  numbers);
+}
+else
+{
+ Console.WriteLine("Число {0} является нечетным ",  numbers);
+}
