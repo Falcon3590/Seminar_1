93e9eb77914bb00b9b7b1b55246da97c05155e14
Falcon3590 <118375336+Falcon3590@users.noreply.github.com>
2022-11-30 12:03:13 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+
+int firstnumber = int.Parse(Console.ReadLine());
+int secondnumber = int.Parse(Console.ReadLine());
+
+if (firstnumber > secondnumber)
+{
+Console.WriteLine("{0} наибольшее число", firstnumber);
+Console.WriteLine("{0} наименьшее число ", secondnumber);
+}
+else
+{
+Console.WriteLine("{0} наибольшее число ", secondnumber);
+Console.WriteLine("{0} наименьшее число ", firstnumber);
+}
